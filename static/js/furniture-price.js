95251257675722071f75f0fcc36b3286fbabf847
Marcus Martin <marclmart95@gmail.com>
2022-02-18 07:03:48 -0600

    dev

--- static/js/furniture-price.js
+++ static/js/furniture-price.js
@@ -16,13 +16,120 @@ let prices = {
         "id": "6swing",
         "name": "6ft Swing or Bench",
         "price": 277
+      },
+      {
+        "id": "2chair",
+        "name": "2ft Chair",
+        "price": 127
+      },
+      {
+        "id": "2glider",
+        "name": "2ft Glider",
+        "price": 214
+      },
+      {
+        "id": "4glider",
+        "name": "4ft Glider",
+        "price": 323
+      },
+      {
+        "id": "5glider",
+        "name": "5ft Glider",
+        "price": 387
+      },
+      {
+        "id": "6glider",
+        "name": "6ft glider",
+        "price": 438
+      },
+      {
+        "id": "snackglider",
+        "name": "Snack Glider",
+        "price": 468
+      },
+      {
+        "id": "minitable",
+        "name": "Mini Table",
+        "price": 104
+      },
+      {
+        "id": "childtable",
+        "name": "Child's Table",
+        "price": 150
+      },
+      {
+        "id": "5table",
+        "name": "5ft Picnic Table",
+        "price": 184
+      },
+      {
+        "id": "6table",
+        "name": "6ft Picnic Table",
+        "price": 196
+      },
+      {
+        "id": "7table",
+        "name": "7ft Picnic Table",
+        "price": 224
+      },
+      {
+        "id": "4arbor",
+        "name": "4ft Arbor",
+        "price": 219
+      },
+      {
+        "id": "5arbor",
+        "name": "5ft Arbor",
+        "price": 276
+      },
+      {
+        "id": "6arbor",
+        "name": "6ft Arbor",
+        "price": 323
+      },
+      {
+        "id": "7arbor",
+        "name": "7ft Arbor",
+        "price": 386
+      },
+      {
+        "id": "8arbor",
+        "name": "8ft Arbor",
+        "price": 438
+      },
+      {
+        "id": "wishingwell",
+        "name": "Wishing Well",
+        "price": 334
+      },
+      {
+        "id": "6bridge",
+        "name": "6ft Lawn Bridge",
+        "price": 311
+      },
+      {
+        "id": "8bridge",
+        "name": "8ft Lawn Bridge",
+        "price": 415
+      },
+      {
+        "id": "10bridge",
+        "name": "10ft Lawn Bridge",
+        "price": 496
+      },
+      {
+        "id": "12bridge",
+        "name": "12ft Lawn Bridge",
+        "price": 600
       }
+
     ],
 
 };
 
 
 const furniturePriceDropdown = document.getElementById('furniture-price');
+const priceEl = document.getElementById('price-number');
 
 let defaultOption = document.createElement('option');
 defaultOption.text = 'Choose Furniture';
@@ -41,3 +148,7 @@ function populateDropdown(){
 }
 
 populateDropdown();
+
+const furniturePrice = function(){
+  priceEl.innerText = prices.furniture[furniturePriceDropdown.value].price
+};
